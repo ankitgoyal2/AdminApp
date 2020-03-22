d678b7eeb92d014f290b576095aacc6700b24c55
Abhinendra <abhinendragaurav@gmail.com>
2020-03-22 20:26:29 +0530

    homepage

---
+++ app/src/main/java/com/example/adminapp/BottomNavigationActivity.java
@@ -0,0 +1,80 @@
+package com.example.adminapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.example.adminapp.fragments.HomeFragment;
+import com.example.adminapp.fragments.ProfileFragment;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+    int old_id = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setOurFragment(new HomeFragment(),1,1);
+        setContentView(R.layout.activity_bottom_navigation);
+        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottom_bar);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_account));
+
+
+
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+
+                switch (item.getId()) {
+                    case 1:
+                        setOurFragment(new HomeFragment(),old_id,1);
+                        old_id = 1;
+                        break;
+
+                    case 2:
+                        setOurFragment(new ProfileFragment(),old_id,2);
+                        old_id = 2;
+                }
+
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(BottomNavigationActivity.this, "showing item : " + item.getId(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(BottomNavigationActivity.this, "reselected item : " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        bottomNavigation.show(1,true);
+    }
+    private void setOurFragment(Fragment fragment, int old_id, int new_id)
+    {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                .beginTransaction();
+        if(old_id<new_id)
+        {
+            fragmentTransaction.setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_left);
+        }
+        else if(new_id<old_id)
+        {
+            fragmentTransaction.setCustomAnimations(R.anim.slide_in_left,R.anim.slide_out_right);
+        }
+
+        fragmentTransaction.replace(R.id.mainframe,fragment);
+        fragmentTransaction.commit();
+    }
+
+}
---
+++ app/src/main/java/com/example/adminapp/adapters/ViewPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.adminapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.adminapp.R;
+
+
+public class ViewPagerAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private int [] images = {R.drawable.generateqrimg,R.drawable.manageuserimg,R.drawable.broadcastmesg};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        layoutInflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.activity_viewpager,null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.img_viewpager);
+        imageView.setImageResource(images[position]);
+
+
+        ViewPager vp =(ViewPager)container;
+        vp.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        ViewPager vp =(ViewPager)container;
+        View view =(View)object;
+        vp.removeView(view);
+    }
+}
