134ad47eebff5344fc540152c1851e6b22d7fb71
Abhinendra <abhinendragaurav@gmail.com>
2020-07-31 17:31:52 +0530

    Stock Record Updated..

--- app/src/main/java/com/example/adminapp/StockRecordActivity.java
+++ app/src/main/java/com/example/adminapp/StockRecordActivity.java
@@ -2,13 +2,26 @@ package com.example.adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 
+import com.example.adminapp.adapters.MachineAdapter;
+import com.example.adminapp.adapters.StockRecordAdpater;
+import com.example.adminapp.models.Machine;
+import com.firebase.ui.database.paging.DatabasePagingOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
 public class StockRecordActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase;
+    RecyclerView recyclerView;
+    StockRecordAdpater adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +39,27 @@ public class StockRecordActivity extends AppCompatActivity {
                 findViewById(R.id.horizontal_progress_bar).setVisibility(View.GONE);
             }
         },4000);
+
+        recyclerView = findViewById(R.id.stock_list_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        Query baseQuery = firebaseDatabase.getReference("Machines");
+
+        PagedList.Config config = new PagedList.Config.Builder()
+                .setEnablePlaceholders(false)
+                .setPrefetchDistance(10)
+                .setPageSize(20)
+                .build();
+
+        DatabasePagingOptions<Machine> options = new DatabasePagingOptions.Builder<Machine>()
+                .setLifecycleOwner(this)
+                .setQuery(baseQuery,config,Machine.class)
+                .build();
+        adapter = new StockRecordAdpater(options,StockRecordActivity.this);
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+
     }
 }
---
+++ app/src/main/java/com/example/adminapp/adapters/StockRecordAdpater.java
@@ -0,0 +1,82 @@
+package com.example.adminapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.adminapp.R;
+import com.example.adminapp.models.Machine;
+import com.firebase.ui.database.paging.DatabasePagingOptions;
+import com.firebase.ui.database.paging.FirebaseRecyclerPagingAdapter;
+import com.firebase.ui.database.paging.LoadingState;
+
+public class StockRecordAdpater extends FirebaseRecyclerPagingAdapter<Machine, StockRecordAdpater.MyMachinesHolder> {
+
+
+
+    Context c;
+    private final int[] mColors = {R.color.list_color_2,R.color.list_color_3,R.color.list_color_4,R.color.list_color_5,
+            R.color.list_color_6,R.color.list_color_7,R.color.list_color_8,R.color.list_color_9,R.color.list_color_10,R.color.list_color_11};
+
+    public StockRecordAdpater(@NonNull DatabasePagingOptions<Machine> options, Context c) {
+        super(options);
+
+        this.c = c;
+    }
+
+
+    @Override
+    protected void onBindViewHolder(@NonNull MyMachinesHolder viewHolder, int position, @NonNull Machine model) {
+        int bgColor = ContextCompat.getColor(c, mColors[position % 10]);
+        viewHolder.cardView.setCardBackgroundColor(bgColor);
+        if(position % 2 ==0){
+            viewHolder.workingStatus.setBackgroundResource(R.drawable.not_working_background);
+        }
+        viewHolder.bind(model);
+    }
+
+    @Override
+    protected void onLoadingStateChanged(@NonNull LoadingState state) {
+
+    }
+
+    @NonNull
+    @Override
+    public MyMachinesHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stock_record_item,null);
+        return new MyMachinesHolder(view);
+    }
+
+    public class MyMachinesHolder extends RecyclerView.ViewHolder {
+
+        TextView machineId, location,type,serialNumber,workingStatus;
+        CardView cardView;
+
+        public MyMachinesHolder(@NonNull View itemView) {
+            super(itemView);
+
+            machineId = itemView.findViewById(R.id.machine_id);
+            serialNumber = itemView.findViewById(R.id.machine_serial_no);
+            location = itemView.findViewById(R.id.machine_location);
+            type = itemView.findViewById(R.id.machine_type);
+            workingStatus = itemView.findViewById(R.id.machine_working_text);
+            cardView = itemView.findViewById(R.id.cardview);
+        }
+
+        public void bind(Machine model)
+        {
+            machineId.setText(model.getMachineId());
+            location.setText(model.getDepartment());
+            type.setText(model.getType());
+            serialNumber.setText(model.getSerialNumber());
+        }
+    }
+}
