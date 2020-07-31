d1eae94f4c7d6d4baf1b5e0ec6568ed43eb23ab8
Vikas Goyal <vikasg.cs.17@nitj.ac.in>
2020-07-31 12:59:32 +0530

    nesting removed

--- app/src/main/java/com/example/adminapp/GenerateQRActivity.java
+++ app/src/main/java/com/example/adminapp/GenerateQRActivity.java
@@ -540,6 +540,9 @@ public class GenerateQRActivity extends AppCompatActivity {
                     Log.i("mail" , manager.getEmail().toString());
                     if (tempManager != null) {
                         tempManager.setMyMachines(null);
+                        tempManager.setCompletedComplaints(null);
+                        tempManager.setPendingApprovalRequest(null);
+                        tempManager.setPendingComplaints(null);
                     }
                     Machine machine = new Machine(serialNo, installationdate, dept, machineId, type, company, model,
                             Objects.requireNonNull(task.getResult()).toString(), servicetime, null, price, true, tempManager, null, scrap, machineLife);
