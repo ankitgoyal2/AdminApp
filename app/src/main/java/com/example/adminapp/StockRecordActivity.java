9b24222637c88d5ee6947eb75cbf18a659d50b2b
Abhinendra <abhinendragaurav@gmail.com>
2020-07-31 14:37:28 +0530

    Stock Record UI

--- app/src/main/java/com/example/adminapp/StockRecordActivity.java
+++ app/src/main/java/com/example/adminapp/StockRecordActivity.java
@@ -1,8 +1,11 @@
 package com.example.adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class StockRecordActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class StockRecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_record);
+        final Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+       getSupportActionBar().setDisplayUseLogoEnabled(true);
+//        ((AppCompatActivity) getActivity()).getSupportActionBar().setLogo(R.drawable.ic_home_toolbar);
+        toolbar.setTitleTextAppearance(this,R.style.TitleTextAppearance);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.horizontal_progress_bar).setVisibility(View.GONE);
+            }
+        },4000);
     }
 }
