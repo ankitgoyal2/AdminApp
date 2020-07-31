dbc40ad9f900b08817d98153a036efe1bbef4212
Abhinendra <abhinendragaurav@gmail.com>
2020-07-31 19:58:36 +0530

    Filter ...

--- app/src/main/java/com/example/adminapp/StockRecordActivity.java
+++ app/src/main/java/com/example/adminapp/StockRecordActivity.java
@@ -1,6 +1,8 @@
 package com.example.adminapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.paging.PagedList;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,24 +10,57 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Adapter;
 
 import com.example.adminapp.adapters.MachineAdapter;
+import com.example.adminapp.adapters.ManagerBasicAdapter;
 import com.example.adminapp.adapters.StockRecordAdpater;
 import com.example.adminapp.models.Machine;
+import com.example.adminapp.models.Manager;
+import com.example.adminapp.models.Tag;
 import com.firebase.ui.database.paging.DatabasePagingOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.yalantis.filter.animator.FiltersListItemAnimator;
+import com.yalantis.filter.listener.FilterListener;
+import com.yalantis.filter.widget.Filter;
 
-public class StockRecordActivity extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockRecordActivity extends AppCompatActivity implements FilterListener<Tag> {
 
     FirebaseDatabase firebaseDatabase;
     RecyclerView recyclerView;
+    private int[] mColors;
+    private String[] mTitles;
+    private List<Machine> mAllMachines;
+    private Filter<Tag> mFilter;
     StockRecordAdpater adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_record);
+
+
+
+        mColors = getResources().getIntArray(R.array.colors);
+        mTitles = getResources().getStringArray(R.array.job_titles);
+
+        mFilter = (Filter<Tag>) findViewById(R.id.filter);
+
+        mFilter.setListener(this);
+
         final Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
@@ -42,24 +77,126 @@ public class StockRecordActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.stock_list_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        firebaseDatabase = FirebaseDatabase.getInstance();
-
-        Query baseQuery = firebaseDatabase.getReference("Machines");
-
-        PagedList.Config config = new PagedList.Config.Builder()
-                .setEnablePlaceholders(false)
-                .setPrefetchDistance(10)
-                .setPageSize(20)
-                .build();
-
-        DatabasePagingOptions<Machine> options = new DatabasePagingOptions.Builder<Machine>()
-                .setLifecycleOwner(this)
-                .setQuery(baseQuery,config,Machine.class)
-                .build();
-        adapter = new StockRecordAdpater(options,StockRecordActivity.this);
-        recyclerView.setAdapter(adapter);
-        adapter.startListening();
 
 
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Machines");
+
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Machine> machineList = new ArrayList<Machine>();
+                for(DataSnapshot ds : dataSnapshot.getChildren())
+                {
+                    machineList.add(ds.getValue(Machine.class));
+                }
+
+                adapter = new StockRecordAdpater(machineList,StockRecordActivity.this);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        recyclerView.setItemAnimator(new FiltersListItemAnimator());
+
+
+    }
+
+    private List<Tag> getTags() {
+        List<Tag> tags = new ArrayList<>();
+
+        for (int i = 0; i < mTitles.length; ++i) {
+            tags.add(new Tag(mTitles[i], mColors[i]));
+        }
+        return tags;
+    }
+
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                adapter.getFilter().filter(query);
+
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText==null || newText.length()==0)
+                {
+                    adapter.getFilter().filter(null);
+
+                    return true;
+                }
+                else {
+                    adapter.getFilter().filter(newText);
+                    return true;
+                }
+
+
+            }
+
+
+
+
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onFilterDeselected(Tag tag) {
+
+    }
+
+    @Override
+    public void onFilterSelected(Tag tag) {
+
+    }
+
+    @Override
+    public void onFiltersSelected(@NotNull ArrayList<Tag> arrayList) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+        if (recyclerView != null) {
+            adapter.setMachine(mAllMachines);
+            adapter.notifyDataSetChanged();
+        }
+    }
+    private List<Machine> findByTags(List<Tag> tags) {
+        List<Machine> machines = new ArrayList<>();
+
+        for (Machine machine : mAllMachines) {
+            for (Tag tag : tags) {
+
+            }
+        }
+
+        return machines;
     }
 }
--- app/src/main/java/com/example/adminapp/adapters/StockRecordAdpater.java
+++ app/src/main/java/com/example/adminapp/adapters/StockRecordAdpater.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,39 +15,31 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.adminapp.R;
 import com.example.adminapp.models.Machine;
+import com.example.adminapp.models.Manager;
 import com.firebase.ui.database.paging.DatabasePagingOptions;
 import com.firebase.ui.database.paging.FirebaseRecyclerPagingAdapter;
 import com.firebase.ui.database.paging.LoadingState;
 
-public class StockRecordAdpater extends FirebaseRecyclerPagingAdapter<Machine, StockRecordAdpater.MyMachinesHolder> {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StockRecordAdpater extends RecyclerView.Adapter<StockRecordAdpater.MyMachinesHolder> implements Filterable {
 
+    private List<Machine> machineList;
+    private List<Machine> machineListFull;
 
     Context c;
     private final int[] mColors = {R.color.list_color_2,R.color.list_color_3,R.color.list_color_4,R.color.list_color_5,
             R.color.list_color_6,R.color.list_color_7,R.color.list_color_8,R.color.list_color_9,R.color.list_color_10,R.color.list_color_11};
 
-    public StockRecordAdpater(@NonNull DatabasePagingOptions<Machine> options, Context c) {
-        super(options);
-
+    public StockRecordAdpater(List<Machine>machineList , Context c) {
+       this.machineList = machineList;
         this.c = c;
+        machineListFull = new ArrayList<>(machineList);
     }
 
 
-    @Override
-    protected void onBindViewHolder(@NonNull MyMachinesHolder viewHolder, int position, @NonNull Machine model) {
-        int bgColor = ContextCompat.getColor(c, mColors[position % 10]);
-        viewHolder.cardView.setCardBackgroundColor(bgColor);
-        if(position % 2 ==0){
-            viewHolder.workingStatus.setBackgroundResource(R.drawable.not_working_background);
-        }
-        viewHolder.bind(model);
-    }
 
-    @Override
-    protected void onLoadingStateChanged(@NonNull LoadingState state) {
-
-    }
 
     @NonNull
     @Override
@@ -54,6 +48,65 @@ public class StockRecordAdpater extends FirebaseRecyclerPagingAdapter<Machine, S
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stock_record_item,null);
         return new MyMachinesHolder(view);
     }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyMachinesHolder holder, int position) {
+        int bgColor = ContextCompat.getColor(c, mColors[position % 10]);
+        holder.cardView.setCardBackgroundColor(bgColor);
+        if(position % 2 ==0){
+            holder.workingStatus.setBackgroundResource(R.drawable.not_working_background);
+        }
+        holder.bind(machineList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return machineList.size();
+    }
+    public void setMachine(List<Machine> machines){
+        this.machineList = machines;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+
+    private Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+
+            List<Machine>filteredList = new ArrayList<Machine>();
+
+            if(constraint==null||constraint.length()==0)
+            {
+                filteredList.addAll(machineListFull);
+            }
+            else
+            {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for(Machine m : machineListFull)
+                {
+                    if(m.getType().toLowerCase().contains(filterPattern))
+                        filteredList.add(m);
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            machineList.clear();
+            machineList.addAll((List)results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public class MyMachinesHolder extends RecyclerView.ViewHolder {
 
